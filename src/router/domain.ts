49c6188f5a4e7a9e448090d2953c0d04ce458e2b
lyswifter <ly70835@163.com>
2023-03-06 13:43:33 +0800

    change doamin

--- src/router/domain.ts
+++ src/router/domain.ts
@@ -1,5 +1,6 @@
 export let domain = {
-    domainUrl: "http://172.16.10.26:8080/api/",
+    // domainUrl: "http://172.16.10.26:8080/api/",
+    domainUrl: "http://18.232.119.81:8080/api/",
 }
 
 export let Apis = {
