b3b128860b21a4f404d8e954e46ce78d58e63151
lyswifter <ly70835@163.com>
2023-03-31 15:52:30 +0800

    add wallet assets

--- src/components/MyWallet.vue
+++ src/components/MyWallet.vue
@@ -3,10 +3,29 @@ import HeaderView from "../components/Header.vue";
 </script>
 
 <template>
-    <HeaderView class="header-view" />
+    <div class="wallet-container">
+        <HeaderView class="header-view" />
 
-    <div>WALLET</div>
+        <div class="top-information-view">
+            <div class="top-inner-view">
+                <img src="../assets/icon_btc@2x.png" alt="" width="140" height="140">
+            </div>
+        </div>
+
+        <div class="insp-content-view">
+        </div>
+    </div>
 </template>
 
 <style scoped>
+.top-information-view {
+    width: 100%;
+    height: 388px;
+    background: linear-gradient(180deg, #4540D6 0%, #BFEAFF 100%);
+}
+
+.top-inner-view {
+    max-width: 1200px;
+    margin: 0 auto;
+}
 </style>
