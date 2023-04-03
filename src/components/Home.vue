95506bae9f73a3b34cbc35b6e82568bdf7263c90
lyswifter <ly70835@163.com>
2023-04-03 13:35:18 +0800

    package patch

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -143,7 +143,7 @@ onMounted(() => {
 
     <div class="slogon-view">
       <img class="solgon-title-view" src="../assets/logo@2x.png" alt="">
-      <div class="solgon-content-view">Search domain name or Register your domain name</div>
+      <div class="solgon-content-view">Discover the Future of BTC Doamin. Search, Register and Trade your .btc Domain Name</div>
     </div>
 
     <div class="search-view">
