c9e4256a63864bca75c19cdf5105d07a507e502a
lyswifter <ly70835@163.com>
2023-04-07 18:46:57 +0800

    add history api and get history data

--- src/components/MyHistory.vue
+++ src/components/MyHistory.vue
@@ -1,4 +1,22 @@
 <script setup lang="ts">
+import { ElMessage } from "element-plus";
+import { onMounted, reactive } from 'vue';
+import openapi from "../crypto/openapi";
+
+let stat = reactive({
+})
+
+onMounted(async () => {
+    let addr = localStorage.getItem('bitcoin_address')
+    if (!addr) {
+        ElMessage.warning("from address must not be empty")
+        return
+    }
+
+    let history = await openapi.getAddressRecentHistory(addr)
+    console.log(history)
+})
+
 </script>
 
 <template>
@@ -7,5 +25,4 @@
     </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
